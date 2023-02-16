89b5b31ce711c023034f0110f2ed2a5c97492808
miftahers <miftahers@gmail.com>
2023-02-16 23:16:25 +0700

    login and register #1
    Co-authored-by: Paqih Teguh Maulana <paqihteguh2324@users.noreply.github.com>

---
+++ route/route.go
@@ -0,0 +1,33 @@
+package route
+
+import (
+	"pocket-message-with-views/controller"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func InitRoute(engine *html.Engine) *fiber.App {
+	// Creating fiber app
+	app := fiber.New(
+		fiber.Config{
+			Views: engine,
+		},
+	)
+
+	//Configure app
+	app.Static("/", "./public")
+
+	// Routes
+	app.Get("/", controller.Index)
+
+	page := app.Group("/page")
+	page.Get("/register", controller.PageRegister)
+	page.Get("/login", controller.PageLogin)
+
+	api := app.Group("/api")
+	auth := api.Group("/auth")
+	auth.Post("/login", controller.Login) //localhost:3000/api/auth/login
+
+	return app
+}
